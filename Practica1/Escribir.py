f7fc49bde17accf7000e08c17caff99ab3acdcf5
yoshiro1786 <66931868+ymirandav@users.noreply.github.com>
2023-03-20 10:36:22 -0500

    Práctica 1 - APIs

---
+++ Practica1/Escribir.py
@@ -0,0 +1,22 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+KEY = 'key.json'
+SPREADSHEET_ID = '1o_zuMxO6_kvAUPP-WtBrmzQxbb3ZjV936-67PTdEAMk'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
+
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+
+# Debe ser una matriz por eso el doble [[]]
+values = [['Vicente']]
+# Llamamos a la api
+result = sheet.values().append(spreadsheetId=SPREADSHEET_ID,
+							range='A1',
+							valueInputOption='USER_ENTERED',
+							body={'values':values}).execute()
+print(f"Datos insertados correctamente.\n{(result.get('updates').get('updatedCells'))}")
---
+++ Practica1/leer.py
@@ -0,0 +1,20 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+KEY = 'key.json'
+# Escribe aquí el ID de tu documento:
+SPREADSHEET_ID = '1o_zuMxO6_kvAUPP-WtBrmzQxbb3ZjV936-67PTdEAMk'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
+
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+
+# Llamada a la api
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:A8').execute()
+# Extraemos values del resultado
+values = result.get('values',[])
+print(values)
